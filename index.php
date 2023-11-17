55f47d712024c036e75e89897a1ddf554b57060a
Ezra Brilliant <64943463+logicliem@users.noreply.github.com>
2023-11-17 21:30:43 +0700

    Update index.php (adding looping to load each product on a card)

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+session_start();
+include 'db_conn.php';
+$stmt = $pdo->query("SELECT * FROM game");
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -7,12 +14,10 @@
     <meta name="description" content="" />
     <meta name="author" content="" />
     <title>Topup Game - Proyek Tekweb</title>
-    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.0/js/bootstrap.bundle.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
-    <link href="css/styles.css" rel="stylesheet" />
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
 </head>
@@ -79,8 +84,34 @@
             </div>
         </div>
     </header>
+    
+    <!-- Game Terbaru-->
+    <section class="bg-dark py-5">
+        <h1 class="text-center text-white">Produk Terbaru</h1>
+        <div class="container px-4 px-lg-5 mt-5">
+            <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                <!-- LOOPING UNTUK LOAD IMAGE DARI DATABASE game -->
+                <?php
+                foreach ($rows as $row) {
+                    $id = $row["game_id"];
+                    echo "<div class='col mb-5' data-aos='fade-up'>";
+                    echo "<div class='card text-white bg-dark mb-3 h-80' style='background-color: rgb(10, 10, 12, 0.4) !important'>";
+                    echo "<img class='card-img-top' src='" . $row['logo'] . "' alt='...' />";
+                    echo "<div class='card-body p-4'>";
+                    echo "<div class='text-center'><h5 class='fw-bolder'>" . $row['game_name'] . "</h5></div>";
+                    echo "</div>";
+                    echo "<div class='card-footer p-4 pt-0 border-top-0 bg-transparent'>";
+                    echo "<div class='text-center'><a href='details.php?id=$id'><button class='btn btn-outline-light' type='submit'>Learn More</button></a></div>";
+                    echo "</div></div></div>";
+                }
+                ?>
+            </div>
+        </div>
+    </section>
+
 </body>
 
+<!-- Footer-->
 <footer class="py-5 bg-dark" style="background-color: rgb(10, 10, 12) !important">
     <div class="container">
         <p class="m-0 text-center text-white">Copyright &copy; PAY2WIN 2023</p>
