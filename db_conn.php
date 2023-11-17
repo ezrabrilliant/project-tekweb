4d33f19fcb16e30e4367955b107d8d864957a2a5
Ezra Brilliant <64943463+logicliem@users.noreply.github.com>
2023-11-17 21:43:12 +0700

    Create db_conn.php

---
+++ db_conn.php
@@ -0,0 +1,8 @@
+<!--
+    password samakan aja 'infor12345' biar gampang
+-->
+<?php
+    $username = 'localhost';
+    $password = 'infor12345';
+    $pdo = new PDO('mysql:host=localhost;dbname=proyek_tekweb', $username, $password);
+?>
