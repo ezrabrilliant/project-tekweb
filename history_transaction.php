084879b2b88bea806e476aeed1bbff0d17b26350
Ezra Brilliant <64943463+ezrabrilliant@users.noreply.github.com>
2023-12-03 03:19:55 +0700

    Update history_transaction.php (Changing Theme)
     (Changing Background Color, Blob Animation, And Dark Transparant Table)

--- history_transaction.php
+++ history_transaction.php
@@ -3,15 +3,13 @@ session_start();
 include 'db_conn.php';
 if (isset($_SESSION['user'])) {
     $user = $_SESSION['user'];
-
-    // Access user information
     $memberId = $user['member_id'];
     $stmt3 = $pdo->prepare("SELECT saldo FROM users WHERE member_id = :member_id");
     $stmt3->bindParam(":member_id", $memberId);
     $stmt3->execute();
     $rows3 = $stmt3->fetch(PDO::FETCH_ASSOC);
-    
-    
+
+
     $userEmail = $user['email'];
     $userName = $user['username'];
     $userTelp = $user['no_telepon'];
@@ -31,12 +29,9 @@ if (isset($_SESSION['user'])) {
     $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 } else {
-    // Redirect or display an error message if the user is not logged in
-    header('location: login.php'); // Redirect to the login page
+    header('location: login.php');
     exit;
 }
-
-
 ?>
 
 
@@ -48,15 +43,20 @@ if (isset($_SESSION['user'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>Topup Game - Proyek Tekweb</title>
-    <!-- Favicon-->
-    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
-    <!-- Bootstrap icons-->
+    <title>Topup Game - PAY2WIN</title>
+    <link rel="icon" href="assets\Web Logo\pay-2-win-full.png" />
+
+
+    <link rel="stylesheet" href="css/styles.css">
+    <script src="js/scripts.js"></script>
+
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
-    <!-- Core theme CSS (includes Bootstrap)-->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/clipboard@2.0.8/dist/clipboard.min.js"></script>
     <script src="https://kit.fontawesome.com/785e8a7b97.js" crossorigin="anonymous"></script>
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
@@ -66,82 +66,79 @@ if (isset($_SESSION['user'])) {
     <script defer src="https://code.jquery.com/jquery-3.7.0.js"></script>
     <script defer src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
     <script defer src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
+
     <style>
-        body {
-            min-height: 100vh;
-            display: flex;
-            flex-direction: column;
-            margin: 0;
-        }
-
-        main {
-            flex-grow: 1;
-            position: relative;
-        }
-
-        footer {
-            margin-top: auto;
+        .table {
+            --bs-table-bg: rgb(0, 0, 0, 0.2) !important;
         }
     </style>
 </head>
 
-<body class="bg-dark">
+<body class="body">
+
+    <!-- Background -->
+    <div class="blob-c">
+        <div class="shape-blob one"></div>
+        <div class="shape-blob two"></div>
+        <div class="shape-blob three"></div>
+        <div class="shape-blob four"></div>
+        <div class="shape-blob five"></div>
+        <div class="shape-blob six"></div>
+    </div>
 
     <!-- Navigation -->
     <nav class="py-4 navbar navbar-expand-lg navbar-dark fixed-top"
-        style="background: rgba(0, 0, 0, 0.6) !important; backdrop-filter: blur(10px) saturate(125%); z-index: 2; -webkit-backdrop-filter: blur(10px) saturate(125%);">
+        style="background: rgba(0, 0, 0, 0.4) !important; backdrop-filter: blur(10px) saturate(125%); z-index: 2; -webkit-backdrop-filter: blur(10px) saturate(125%);">
         <div class="container px-4 px-lg-5 text-white">
-            <a class="navbar-brand" href="home.php">
-                <h3>PAY2WIN</h3>
+            <a class="navbar-brand" style="height: 52px;" href="home.php">
+                <img src="assets\Web Logo\pay-2-win-full.png" alt="PAY2WIN Logo" class="img-fluid"
+                    style="max-height: 50px;margin-top: -2px;height: 100%;object-fit: cover;">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-                    <li class="nav-item me-4"><a class="nav-link" aria-current="page" href="home.php">HOME</a>
-                    </li>
-                    <li class="nav-item me-4"><a class="nav-link" href="about.php">ABOUT</a></li>
-                    <li class="nav-item me-4"><a class="nav-link active" href="history_transaction.php">HISTORY
-                            TRANSACTION</a>
-                    </li>
-                    <li class="nav-item me-4"><button type="button" class="balance-info-btn btn btn-outline-light"
-                            data-id="<?php echo $memberId; ?>" data-saldo="<?php echo $saldo; ?>" data-bs-toggle="modal"
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4 align-items-center">
+                    <li class="nav-item me-lg-4 d-lg-block"><a class="nav-link" aria-current="page"
+                            href="home.php">HOME</a></li>
+                    <li class="nav-item me-lg-4 d-lg-block"><a class="nav-link" href="about.php">ABOUT</a></li>
+                    <li class="nav-item me-lg-4 d-lg-block"><a class="nav-link active"
+                            href="history_transaction.php">HISTORY TRANSACTION</a></li>
+                    <li class="nav-item me-lg-4 d-lg-block"><button type="button"
+                            class="balance-info-btn btn btn-outline-light" data-id="<?php echo $memberId; ?>"
+                            data-saldo="<?php echo $saldo; ?>" data-bs-toggle="modal"
                             data-bs-target="#balanceInfo">BALANCE & TOP UP</button></li>
                 </ul>
 
                 </ul>
                 <ul class="navbar-nav align-items-center">
-                    <li class="nav-item">
-                        <a class="nav-link me-3" href="#">
+                    <li class="nav-item me-lg-4 d-lg-block"><a class="nav-link" href="#">
                             <p class="mb-0">Welcome,
                                 <?php echo $userName; ?>
                             </p>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#">
-                            <button type="button" class="btn btn-outline-light" data-bs-toggle="modal"
-                                data-bs-target="#logoutModal">Log Out</button>
-                        </a>
-                    </li>
+                        </a></li>
+                    <li class="nav-item me-lg-4 d-lg-block"><a href="#"><button type="button"
+                                class="btn btn-outline-light" data-bs-toggle="modal" data-bs-target="#logoutModal">Log
+                                Out</button></a></li>
                 </ul>
             </div>
         </div>
     </nav>
 
-
+    <!-- Table -->
     <div id="wrapper" style='margin-top: 150px !important; margin-bottom: 40px !important;'>
         <div id="content-wrapper" class="d-flex flex-column">
             <div id="content">
                 <div class="container" style="padding: 40px 0 20px 0;">
-                    <div class="card mb-4">
+                    <div class="card animate border-black text-white mb-4"
+                        style='border-radius: 1rem;background-color: rgb(0, 0, 0, 0.4) !important'>
                         <div class="card-header py-3">
-                            <h6 class="m-0 font-weight-bold text-dark"><strong>Riwayat Pembelian User</strong></h6>
+                            <h6 class="m-0 font-weight-bold"><strong>Riwayat Pembelian User</strong></h6>
                         </div>
                         <div class="card-body">
                             <div class="table-responsive">
-                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                <table class="table table-bordered table-dark" id="dataTable" width="100%"
+                                    cellspacing="0">
                                     <thead>
                                         <tr>
                                             <th>Invoice ID</th>
@@ -219,7 +216,8 @@ if (isset($_SESSION['user'])) {
         </div>
     </div>
 
-    <!-- Modal Balance & top up balance -->
+
+    <!--- Modal Balance & top up balance -->
     <div class="modal fade" id="balanceInfo" tabindex="-1" role="dialog" aria-labelledby="addAdminModalLabel"
         aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
@@ -229,23 +227,17 @@ if (isset($_SESSION['user'])) {
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-
                     <form id="topUpForm">
                         <h5>Current Balance: Rp.
                             <?php echo number_format($saldo, 2, ',', '.') ?>
                         </h5>
-
-
                         <p>Input nominal isi saldo wallet mu</p>
-
                         <div class="input-group mb-3">
                             <span class="input-group-text" id="basic-addon1">Rp.</span>
-                            <input type="number" class="form-control" placeholder="0" name="amount" aria-label="Username"
-                                aria-describedby="basic-addon1">
+                            <input type="number" class="form-control" placeholder="0" aria-label="Username"
+                                id="inputAmount" name="inputAmount" required aria-describedby="basic-addon1">
                         </div>
-
                         <input type="hidden" id="curBalance" name="curBalance">
-                        <input type="hidden" id="user_id" name="user_id">
 
                         <div class="text-center d-flex justify-content-between" style="padding:10px 0 0 0">
                             <button type="button" class="btn text-white btn-outline-danger" data-bs-dismiss="modal"
@@ -253,83 +245,195 @@ if (isset($_SESSION['user'])) {
                             <input type="submit" class="btn text-white btn-outline-success w-25 login" value="Pay"
                                 name="login-btn">
                         </div>
-
                         <?php if (!empty($errorMsg)): ?>
                             <p class="text-danger text-center mt-3">
                                 <?php echo $errorMsg; ?>
                             </p>
                         <?php endif; ?>
                     </form>
+                    <!-- Payment Details Section (Initially Hidden) -->
+                    <div id="paymentDetails" style="display: none;">
+                        <h5>Payment Details:</h5>
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-8">
+                                    <div class="d-flex align-items-center flex-wrap">
+                                        <span>BCA: &nbsp;</span><span id="copy1">0234567999</span>
+                                        <button class="btn btn-sm btn-outline-light" style="margin-left:10px"
+                                            data-clipboard-target="#copy1">
+                                            <i class="fa-solid fa-copy"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <p>Total Amount: Rp. <span id="totalAmount"></span></p>
+                        <form id="confirmForm">
+                            <input type="hidden" id="amount" name="amount">
+                            <input type="hidden" id="user_id" name="user_id">
+                            <input type="submit" class="btn text-white btn-outline-success w-100 login" value="Confirm">
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 
+    </script>
 
-
-
-    <!-- script -->
     <script>
 
-        $(document).ready(function () {
+        // Logout Script
+        function logout() {
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', 'logout.php', true);
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState == 4 && xhr.status == 200) {
+                    window.location.href = 'index.php';
+                }
+            };
+            xhr.send();
+        }
 
+        // Balance Info Script
+        $(document).ready(function () {
             $('.balance-info-btn').on('click', function () {
                 var memberId = $(this).data('id');
                 var saldo = $(this).data('saldo');
 
                 console.log(memberId);
                 console.log(saldo);
-                // Mengisi nilai input modal edit dengan data pengguna yang akan diedit
                 $('#user_id').val(memberId);
                 $('#curBalance').val(saldo);
-
-                // Menampilkan modal edit
                 $('#balanceInfo').modal('show');
             });
+        });
 
+        
+        $('#balanceInfo').on('hidden.bs.modal', function () {
+            location.reload();
+        });
+
+        
+        // Top Up Script
+        $(document).ready(function () {
             $('#topUpForm').on('submit', function (event) {
+                event.preventDefault();
+
+                var amount = parseFloat($(this).find('input[name="inputAmount"]').val());
+                if (isNaN(amount) || amount <= 0) {
+                    $('#alertContainer').html('<div class="alert alert-danger alert-dismissible fade show" role="alert">Please enter a valid amount.</div>');
+                    return false;
+                }
+                var formattedAmount = new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(amount);
+
+                $('#totalAmount').text(formattedAmount);
+
+
+                amount = document.getElementById('inputAmount').value;
+                console.log(amount);
+                $('#paymentDetails').show();
+                $('#amount').val(amount);
+
+                $(this).find('input[name="inputAmount"]').prop('disabled', true);
+                $(this).find('input[name="login-btn"]').prop('disabled', true);
+
+            });
+
+            $('#topUpForm').on('click', 'input[name="login-btn"]', function () {
+                $('#topUpForm').submit();
+            });
+
+            $('#confirmForm').on('submit', function (event) {
                 event.preventDefault();
                 $.ajax({
                     type: 'post',
-                    url: 'request_saldo.php', // Ganti dengan nama file yang sesuai di server Anda
+                    url: 'request_saldo.php',
                     data: $(this).serialize(),
                     success: function (response) {
-                        // Tindakan setelah perubahan pengguna disimpan (misalnya, menutup modal)
                         $('#editGameModal').modal('hide');
                         alert('Permintaan isi saldo sudah dikirim ke admin kami, harap tunggu beberapa saat :).');
-                        // Refresh halaman untuk memperbarui tampilan riwayat pembelian pengguna
                         location.reload();
 
                     },
                     error: function (xhr, status, error) {
-                        // Tindakan jika penyimpanan perubahan gagal
                         console.error('Terjadi kesalahan: ' + error);
                     }
                 });
             });
-
-            $(document).ready(function () {
-                $('#dataTable').DataTable();
-            });
-
         });
 
-        function logout() {
-            // Buat permintaan AJAX ke server untuk menghapus sesi
-            var xhr = new XMLHttpRequest();
-            xhr.open('GET', 'logout.php', true);
-            xhr.onreadystatechange = function () {
-                if (xhr.readyState == 4 && xhr.status == 200) {
-                    // Jika sesi dihapus dengan sukses, redirect ke halaman index.php
-                    window.location.href = 'index.php';
-                }
+        //Copy Button Script
+        const target = document.getElementById('copy1');
+        const button = target.nextElementSibling;
+        const clipboard = new ClipboardJS(button);
+        clipboard.on('success', function (e) {
+            var checkIcon = button.querySelector('.ki-check');
+            var copyIcon = button.querySelector('.ki-copy');
+            if (checkIcon) {
+                return;
+            }
+        });
+        //Script Animation Card 
+        document.addEventListener("DOMContentLoaded", function () {
+            var options = {
+                root: null,
+                rootMargin: "0px",
+                threshold: 0.5
             };
-            xhr.send();
-        }
+
+            var observer = new IntersectionObserver(function (entries, observer) {
+                entries.forEach(function (entry) {
+                    if (entry.isIntersecting) {
+                        entry.target.classList.add("animate");
+                        observer.unobserve(entry.target);
+                    }
+                });
+            }, options);
+
+            var elements = document.querySelectorAll(".card");
+
+            elements.forEach(function (element) {
+                observer.observe(element);
+            });
+        });
+
+
+
+        $(document).ready(function () {
+            $('#dataTable').DataTable();
+        });
+
+        // Animate Card
+        document.addEventListener("DOMContentLoaded", function () {
+            var options = {
+                root: null,
+                rootMargin: "0px",
+                threshold: 0.5
+            };
+
+            var observer = new IntersectionObserver(function (entries, observer) {
+                entries.forEach(function (entry) {
+                    if (entry.isIntersecting) {
+                        entry.target.classList.add("animate");
+                        observer.unobserve(entry.target);
+                    }
+                });
+            }, options);
+
+            var elements = document.querySelectorAll(".card");
+
+            elements.forEach(function (element) {
+                observer.observe(element);
+            });
+        });
     </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+
 </body>
 
-<footer class="py-5 bg-dark" style="background-color: rgb(10, 10, 12) !important">
+<!-- Footer-->
+<footer class="py-5 bg-dark" style="background-color: rgb(0, 0, 0, 0.4) !important">
     <div class="container">
         <p class="m-0 text-center text-white">Copyright &copy; PAY2WIN 2023</p>
     </div>
