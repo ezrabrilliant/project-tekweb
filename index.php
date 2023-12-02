eb4cd53cfa7e0ee6e24d4d0929ecdf3d0d53b3e4
Ezra Brilliant <64943463+ezrabrilliant@users.noreply.github.com>
2023-12-03 03:21:57 +0700

    Update index.php  (Changing Background Color and Blob Animation)

--- index.php
+++ index.php
@@ -51,111 +51,102 @@ $rows1 = $stmt1->fetchAll(PDO::FETCH_ASSOC);
     <meta name="description" content="" />
     <meta name="author" content="" />
     <title>Topup Game - Proyek Tekweb</title>
+    <link rel="icon" href="assets\Web Logo\pay-2-win-full.png" />
+    
+    <link rel="stylesheet" href="css/styles.css">
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    
     <style>
-        @keyframes fadeIn {
-            from {
-                opacity: 0;
-                transform: translateY(20px);
-            }
-
-            to {
-                opacity: 1;
-                transform: translateY(0);
-            }
-        }
-
-        .card {
+    @keyframes fadeIn {
+        from {
             opacity: 0;
+            transform: translateY(20px);
         }
 
-        .animate {
-            animation: fadeIn 0.8s ease-out;
-            animation-fill-mode: forwards;
+        to {
+            opacity: 1;
+            transform: translateY(0);
         }
+    }
 
-        body {
-            min-height: 100vh;
-            display: flex;
-            flex-direction: column;
-            margin: 0;
-        }
+    .card {
+        opacity: 0;
+    }
 
-        main {
-            flex-grow: 1;
-        }
-
-        footer {
-            margin-top: auto;
-        }
-
-        #carouselExampleIndicators {
-            border-radius: 1rem;
-            overflow: hidden;
-            /* Ensures rounded corners are visible */
-        }
-
-        .carousel-inner {
-            border-radius: 1rem;
-            /* Applies border radius to the images inside the carousel */
-        }
-
-        .carousel-item img {
-            border-radius: 1rem;
-            /* Applies border radius to individual carousel images */
-        }
+    .animate {
+        animation: fadeIn 0.8s ease-out;
+        animation-fill-mode: forwards;
+    }
+    
+    #carouselExampleIndicators {
+        border-radius: 1rem;
+        overflow: hidden;
+    }
     </style>
 </head>
 
-<body class="body bg-dark">
+<body class="body">
+
+    <div class="blob-c">
+        <div class="shape-blob one"></div>
+        <div class="shape-blob two"></div>
+        <div class="shape-blob three"></div>
+        <div class="shape-blob four"></div>
+        <div class="shape-blob five"></div>
+        <div class="shape-blob six"></div>
+    </div>
 
     <!-- Navigation -->
     <nav class="py-4 navbar navbar-expand-lg navbar-dark fixed-top"
-        style="background: rgba(0, 0, 0, 0.6) !important; backdrop-filter: blur(10px) saturate(125%); z-index: 2; -webkit-backdrop-filter: blur(10px) saturate(125%);">
+        style="background: rgba(0, 0, 0, 0.4) !important; backdrop-filter: blur(10px) saturate(125%); z-index: 2; -webkit-backdrop-filter: blur(10px) saturate(125%);">
         <div class="container px-4 px-lg-5 text-white">
-            <a class="navbar-brand" href="index.php">
-                <h3>PAY2WIN</h3>
+            <a class="navbar-brand" style="height: 52px;" href="index.php">
+                <img src="assets\Web Logo\pay-2-win-full.png" alt="PAY2WIN Logo" class="img-fluid"
+                    style="max-height: 50px;margin-top: -2px;height: 100%;object-fit: cover;">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+            <div class="collapse navbar-collapse align-items-center" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4 align-items-center">
                     <li class="nav-item me-4"><a class="nav-link active" aria-current="page" href="index.php">HOME</a>
                     </li>
                     <li class="nav-item me-4"><a class="nav-link" href="about.php">ABOUT</a></li>
                 </ul>
+
                 <ul class="navbar-nav align-items-center">
-                    <li class="nav-item me-3"><a href="login.php" class="nav-link"><button class="btn btn-outline-light"
-                                type="submit">Login</button></a></li>
-                    <li class="nav-item"><a href="signup.php" class="nav-link"><button class="btn btn-outline-light"
-                                type="submit">Sign up</button></a></li>
+                    <li class="nav-item me-4">
+                        <a href="login.php" class="nav-link">
+                            <button class="btn btn-outline-light" type="submit">Login</button>
+                        </a>
+                    </li>
+                    <li class="nav-item me-4">
+                        <a href="signup.php" class="nav-link">
+                            <button class="btn btn-outline-light" type="submit">Sign up</button>
+                        </a>
+                    </li>
                 </ul>
             </div>
         </div>
     </nav>
 
-
     <!-- Header-->
-    <header class="py-5">
+    <header class="py-3">
         <div class="container px-4 px-lg-5" style="margin-top: 150px !important; margin-bottom: 40px !important;">
             <div id="carouselExampleIndicators" class="carousel slide">
-                <div class="carousel-indicators">
+                <div class="carousel-indicators" style="z-index: 1;">
                     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0"
                         class="active" aria-current="true" aria-label="Slide 1"></button>
                     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
                         aria-label="Slide 2"></button>
                 </div>
-                <div class="carousel-inner">
+                <div class="carousel-inner" style="border-radius: 1rem;">
                     <div class="carousel-item active" data-bs-interval="3000">
-                        <img src="assets/coc_home.jpg" class="d-block w-100" alt="...">
+                        <img src="assets/coc_home.jpg" class="d-block w-100" alt="..." >
                     </div>
                     <div class="carousel-item" data-bs-interval="3000">
                         <img src="assets/hsr_banner.jpg" class="d-block w-100" alt="...">
@@ -181,35 +172,24 @@ $rows1 = $stmt1->fetchAll(PDO::FETCH_ASSOC);
         <div class="container px-4 px-lg-5 mt-5">
             <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
                 <!-- LOOPING UNTUK LOAD IMAGE DARI DATABASE game -->
-                <?php foreach ($rows as $row): ?>
-                    <?php
+                <?php
+                foreach ($rows as $row) {
                     $id = $row["game_id"];
-                    ?>
-                    <div class='col mb-5' data-aos='fade-up'>
-                        <div class='card text-white bg-dark mb-3 h-80'
-                            style='border-radius: 1rem;background-color: rgb(10, 10, 12, 0.4) !important'>
-                            <img class='card-img-top' src='<?= $row['logo'] ?>' alt='...' />
-                            <div class='card-body p-4'>
-                                <div class='text-center'>
-                                    <h5 class='card-title'>
-                                        <a>
-                                            <?= $row['game_name'] ?>
-                                        </a>
-                                    </h5>
-                                </div>
-                            </div>
-                            <div class='card-footer p-4 pt-0 border-top-0 bg-transparent'>
-                                <div class='text-center'><a href='details.php?id=<?= $id ?>'><button
-                                            class='btn btn-outline-light stretched-link' type='submit'>Learn
-                                            More</button></a></div>
-                            </div>
-                        </div>
+                    echo "
+                    <div class='col mb-5'>
+                    <div class='card border-black text-white mb-3 h-80' style='border-radius: 1rem;background-color: rgb(0, 0, 0, 0.4) !important'>
+                    <img class='card-img-top' style='border-radius: 1rem;' src='" . $row['logo'] . "' alt='...' />
+                    <div class='card-body p-4'>
+                    <div class='text-center'><h5 class='fw-bolder'>" . $row['game_name'] . "</h5></div>
                     </div>
-                <?php endforeach; ?>
+                    <div class='card-footer p-4 pt-0 border-top-0 bg-transparent'>
+                    <div class='text-center'><a href='details.php?id=$id'><button class='btn btn-outline-light stretched-link' type='submit'>Learn More</button></a></div>
+                    </div></div></div>";
+                }
+                ?>
             </div>
         </div>
     </section>
-
 
     <!-- Paling Laris -->
     <section class="py-5">
@@ -217,33 +197,23 @@ $rows1 = $stmt1->fetchAll(PDO::FETCH_ASSOC);
         <div class="container px-4 px-lg-5 mt-5">
             <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
                 <!-- Looping to load images and game names from the database -->
-                <?php foreach ($rows1 as $row1): ?>
-                    <?php
+                <?php
+                foreach ($rows1 as $row1) {
                     $id = $row1["game_id"];
-                    ?>
-                    <div class='col mb-5' data-aos='fade-up'>
-                        <div class='card text-white bg-dark mb-3 h-80'
-                            style='border-radius: 1rem;background-color: rgb(10, 10, 12, 0.4) !important'>
-                            <img class='card-img-top' src='<?= $row1['logo'] ?>' alt='...' />
-                            <div class='card-body p-4'>
-                                <div class='text-center'>
-                                    <h5 class='fw-bolder'>
-                                        <?= $row1['game_name'] ?>
-                                    </h5>
-                                </div>
-                            </div>
-                            <div class='card-footer p-4 pt-0 border-top-0 bg-transparent'>
-                                <div class='text-center'><a href='details.php?id=<?= $id ?>'><button
-                                            class='btn btn-outline-light stretched-link' type='submit'>Learn
-                                            More</button></a></div>
-                            </div>
-                        </div>
+                    echo "<div class='col mb-5'>
+                    <div class='card border-black text-white mb-3 h-80' style='border-radius: 1rem;background-color: rgb(0, 0, 0, 0.4) !important'>
+                    <img class='card-img-top' style='border-radius: 1rem;' src='" . $row1['logo'] . "' alt='...' />
+                    <div class='card-body p-4'>
+                    <div class='text-center'><h5 class='fw-bolder'>" . $row1['game_name'] . "</h5></div>
                     </div>
-                <?php endforeach; ?>
+                    <div class='card-footer p-4 pt-0 border-top-0 bg-transparent'>
+                    <div class='text-center'><a href='details.php?id=$id'><button class='btn btn-outline-light  stretched-link' type='submit'>Learn More</button></a></div>
+                    </div></div></div>";
+                }
+                ?>
             </div>
         </div>
     </section>
-
 
     <!--Script Animation Card-->
     <script>
@@ -273,7 +243,7 @@ $rows1 = $stmt1->fetchAll(PDO::FETCH_ASSOC);
 
     <!-- Bootstrap core JS-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-    
+
     <!-- Script auto next script -->
     <script>
         document.addEventListener('DOMContentLoaded', function () {
@@ -292,7 +262,7 @@ $rows1 = $stmt1->fetchAll(PDO::FETCH_ASSOC);
 </body>
 
 <!-- Footer-->
-<footer class="py-5 bg-dark" style="background-color: rgb(10, 10, 12) !important">
+<footer class="py-5 bg-dark" style="background-color: rgb(0, 0, 0, 0.4) !important">
     <div class="container">
         <p class="m-0 text-center text-white">Copyright &copy; PAY2WIN 2023</p>
     </div>
