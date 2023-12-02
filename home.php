a965ab9ac985ee86b5f5e34c07a4103470a32611
Ezra Brilliant <64943463+ezrabrilliant@users.noreply.github.com>
2023-12-03 03:20:10 +0700

    Update home.php  (Changing Background Color and Blob Animation)

--- home.php
+++ home.php
@@ -55,12 +55,6 @@ if (isset($_SESSION['user'])) {
     $userName = $user['username'];
     $password = $user['password'];
     $saldo = $user['saldo'];
-
-
-    // Add more fields as needed
-
-    // Display user information
-    // echo "Welcome, $userName! Your email is $userEmail.";
 } else {
     // Redirect or display an error message if the user is not logged in
     header('location: login.php'); // Redirect to the login page
@@ -77,9 +71,10 @@ if (isset($_SESSION['user'])) {
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
-    <title>Topup Game - Proyek Tekweb</title>
+    <title>Topup Game - PAY2WIN</title>
+    <link rel="icon" href="assets\Web Logo\pay-2-win-full.png" />
+
+    <link rel="stylesheet" href="css/styles.css">
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
@@ -108,27 +103,6 @@ if (isset($_SESSION['user'])) {
             animation-fill-mode: forwards;
         }
 
-        body {
-            min-height: 100vh;
-            display: flex;
-            flex-direction: column;
-            margin: 0;
-        }
-
-        main {
-            flex-grow: 1;
-        }
-
-        footer {
-            margin-top: auto;
-        }
-
-        input::-webkit-outer-spin-button,
-        input::-webkit-inner-spin-button {
-            -webkit-appearance: none;
-            margin: 0;
-        }
-
         #carouselExampleIndicators {
             border-radius: 1rem;
             overflow: hidden;
@@ -136,11 +110,20 @@ if (isset($_SESSION['user'])) {
     </style>
 </head>
 
-<body class="bg-dark">
+<body class="body">
+
+    <div class="blob-c">
+        <div class="shape-blob one"></div>
+        <div class="shape-blob two"></div>
+        <div class="shape-blob three"></div>
+        <div class="shape-blob four"></div>
+        <div class="shape-blob five"></div>
+        <div class="shape-blob six"></div>
+    </div>
 
     <!-- Navigation -->
     <nav class="py-4 navbar navbar-expand-lg navbar-dark fixed-top"
-        style="background: rgba(0, 0, 0, 0.6) !important; backdrop-filter: blur(10px) saturate(125%); z-index: 2; -webkit-backdrop-filter: blur(10px) saturate(125%);">
+        style="background: rgba(0, 0, 0, 0.4) !important; backdrop-filter: blur(10px) saturate(125%); z-index: 2; -webkit-backdrop-filter: blur(10px) saturate(125%);">
         <div class="container px-4 px-lg-5 text-white">
             <a class="navbar-brand" style="height: 52px;" href="home.php">
                 <img src="assets\Web Logo\pay-2-win-full.png" alt="PAY2WIN Logo" class="img-fluid"
@@ -149,48 +132,45 @@ if (isset($_SESSION['user'])) {
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-                    <li class="nav-item me-4"><a class="nav-link active" aria-current="page" href="home.php">HOME</a>
-                    </li>
-                    <li class="nav-item me-4"><a class="nav-link" href="about.php">ABOUT</a></li>
-                    <li class="nav-item me-4"><a class="nav-link" href="history_transaction.php">HISTORY TRANSACTION</a>
-                    </li>
-                    <li class="nav-item me-4"><button type="button" class="balance-info-btn btn btn-outline-light"
-                            data-id="<?php echo $memberId; ?>" data-saldo="<?php echo $saldo; ?>" data-bs-toggle="modal"
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4 align-items-center">
+                    <li class="nav-item me-lg-4 d-lg-block"><a class="nav-link active" aria-current="page"
+                            href="home.php">HOME</a></li>
+                    <li class="nav-item me-lg-4 d-lg-block"><a class="nav-link" href="about.php">ABOUT</a></li>
+                    <li class="nav-item me-lg-4 d-lg-block"><a class="nav-link" href="history_transaction.php">HISTORY
+                            TRANSACTION</a></li>
+                    <li class="nav-item me-lg-4 d-lg-block"><button type="button"
+                            class="balance-info-btn btn btn-outline-light" data-id="<?php echo $memberId; ?>"
+                            data-saldo="<?php echo $saldo; ?>" data-bs-toggle="modal"
                             data-bs-target="#balanceInfo">BALANCE & TOP UP</button></li>
                 </ul>
 
-                </ul>
                 <ul class="navbar-nav align-items-center">
-                    <li class="nav-item">
-                        <a class="nav-link me-3" href="#">
+                    <li class="nav-item me-lg-4 d-lg-block"><a class="nav-link" href="#">
                             <p class="mb-0">Welcome,
                                 <?php echo $userName; ?>
                             </p>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#">
-                            <button type="button" class="btn btn-outline-light" data-bs-toggle="modal"
-                                data-bs-target="#logoutModal">Log Out</button>
-                        </a>
-                    </li>
+                        </a></li>
+                    <li class="nav-item me-lg-4 d-lg-block"><a href="#"><button type="button"
+                                class="btn btn-outline-light" data-bs-toggle="modal" data-bs-target="#logoutModal">Log
+                                Out</button></a></li>
                 </ul>
             </div>
         </div>
     </nav>
 
-
     <!-- Header-->
-    <header class="bg-dark py-5">
+    <header class="py-5">
         <div class="container px-4 px-lg-5" style="margin-top: 150px !important; margin-bottom: 40px !important;">
             <div id="carouselExampleIndicators" class="carousel slide">
-                <div class="carousel-indicators">
+                <div class="carousel-indicators" style="z-index: 1;">
                     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0"
                         class="active" aria-current="true" aria-label="Slide 1"></button>
                     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
                         aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+                        aria-label="Slide 3"></button>
                 </div>
                 <div class="carousel-inner" style="border-radius: 1rem;">
                     <div class="carousel-item active" data-bs-interval="3000">
@@ -198,6 +178,9 @@ if (isset($_SESSION['user'])) {
                     </div>
                     <div class="carousel-item" data-bs-interval="3000">
                         <img src="assets/hsr_banner.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item" data-bs-interval="3000">
+                        <img src="assets/banner3.jpeg" class="d-block w-100" alt="...">
                     </div>
                 </div>
                 <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
@@ -224,15 +207,16 @@ if (isset($_SESSION['user'])) {
                 foreach ($rows as $row) {
                     $id = $row["game_id"];
                     //echo($id);
-                    echo "<div class='col mb-5'>";
-                    echo "<div class='card text-white mb-3 h-80' style='border-radius: 1rem;background-color: rgb(10, 10, 12, 0.4) !important'>";
-                    echo "<img class='card-img-top' style='border-radius: 1rem;' src='" . $row['logo'] . "' alt='...' />";
-                    echo "<div class='card-body p-4'>";
-                    echo "<div class='text-center'><h5 class='fw-bolder'>" . $row['game_name'] . "</h5></div>";
-                    echo "</div>";
-                    echo "<div class='card-footer p-4 pt-0 border-top-0 bg-transparent'>";
-                    echo "<div class='text-center'><a href='details.php?id=$id'><button class='btn btn-outline-light stretched-link' type='submit'>Learn More</button></a></div>";
-                    echo "</div></div></div>";
+                    echo "
+                    <div class='col mb-5'>
+                    <div class='card border-black text-white mb-3 h-80' style='border-radius: 1rem;background-color: rgb(0, 0, 0, 0.4) !important'>
+                    <img class='card-img-top' style='border-radius: 1rem;' src='" . $row['logo'] . "' alt='...' />
+                    <div class='card-body p-4'>
+                    <div class='text-center'><h5 class='fw-bolder'>" . $row['game_name'] . "</h5></div>
+                    </div>
+                    <div class='card-footer p-4 pt-0 border-top-0 bg-transparent'>
+                    <div class='text-center'><a href='details.php?id=$id'><button class='btn btn-outline-light stretched-link' type='submit'>Learn More</button></a></div>
+                    </div></div></div>";
                 }
                 ?>
             </div>
@@ -241,22 +225,22 @@ if (isset($_SESSION['user'])) {
 
 
     <!-- Paling Laris -->
-    <section class="bg-dark py-5">
+    <section class="py-5">
         <h1 class="text-center text-white">Paling Laris</h1>
         <div class="container px-4 px-lg-5 mt-5">
             <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
                 <?php
                 foreach ($rows1 as $row1) {
                     $id = $row1["game_id"];
-                    echo "<div class='col mb-5'>";
-                    echo "<div class='card text-white bg-dark mb-3 h-80' style='border-radius: 1rem;background-color: rgb(10, 10, 12, 0.4) !important'>";
-                    echo "<img class='card-img-top' style='border-radius: 1rem;' src='" . $row1['logo'] . "' alt='...' />";
-                    echo "<div class='card-body p-4'>";
-                    echo "<div class='text-center'><h5 class='fw-bolder'>" . $row1['game_name'] . "</h5></div>";
-                    echo "</div>";
-                    echo "<div class='card-footer p-4 pt-0 border-top-0 bg-transparent'>";
-                    echo "<div class='text-center'><a href='details.php?id=$id'><button class='btn btn-outline-light  stretched-link' type='submit'>Learn More</button></a></div>";
-                    echo "</div></div></div>";
+                    echo "<div class='col mb-5'>
+                    <div class='card border-black text-white mb-3 h-80' style='border-radius: 1rem;background-color: rgb(0, 0, 0, 0.4) !important'>
+                    <img class='card-img-top' style='border-radius: 1rem;' src='" . $row1['logo'] . "' alt='...' />
+                    <div class='card-body p-4'>
+                    <div class='text-center'><h5 class='fw-bolder'>" . $row1['game_name'] . "</h5></div>
+                    </div>
+                    <div class='card-footer p-4 pt-0 border-top-0 bg-transparent'>
+                    <div class='text-center'><a href='details.php?id=$id'><button class='btn btn-outline-light  stretched-link' type='submit'>Learn More</button></a></div>
+                    </div></div></div>";
                 }
                 ?>
             </div>
@@ -299,10 +283,10 @@ if (isset($_SESSION['user'])) {
                         <div class="input-group mb-3">
                             <span class="input-group-text" id="basic-addon1">Rp.</span>
                             <input type="number" class="form-control" placeholder="0" aria-label="Username"
-                                id="inputAmount" name="inputAmount" required aria-describedby="basic-addon1" require>
+                                id="inputAmount" name="inputAmount" required aria-describedby="basic-addon1">
                         </div>
                         <input type="hidden" id="curBalance" name="curBalance">
-                       
+
                         <div class="text-center d-flex justify-content-between" style="padding:10px 0 0 0">
                             <button type="button" class="btn text-white btn-outline-danger" data-bs-dismiss="modal"
                                 aria-label="Close">Cancel</button>
@@ -335,7 +319,7 @@ if (isset($_SESSION['user'])) {
                         <form id="confirmForm">
                             <input type="hidden" id="amount" name="amount">
                             <input type="hidden" id="user_id" name="user_id">
-                            <input type="submit" class="btn text-white btn-outline-success w-25 login" value="Confirm">
+                            <input type="submit" class="btn text-white btn-outline-success w-100 login" value="Confirm">
                         </form>
                     </div>
                 </div>
@@ -343,59 +327,52 @@ if (isset($_SESSION['user'])) {
         </div>
     </div>
 
-    <!-- Bootstrap core JS-->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-
-
     <!-- script -->
     <script>
 
-        $(document).ready(function () {
+        // Logout Script
+        function logout() {
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', 'logout.php', true);
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState == 4 && xhr.status == 200) {
+                    window.location.href = 'index.php';
+                }
+            };
+            xhr.send();
+        }
 
+        // Balance Info Script
+        $(document).ready(function () {
             $('.balance-info-btn').on('click', function () {
                 var memberId = $(this).data('id');
                 var saldo = $(this).data('saldo');
 
                 console.log(memberId);
                 console.log(saldo);
-                // Mengisi nilai input modal edit dengan data pengguna yang akan diedit
                 $('#user_id').val(memberId);
                 $('#curBalance').val(saldo);
-
-                // Menampilkan modal edit
                 $('#balanceInfo').modal('show');
             });
-
-
         });
 
-        function logout() {
-            // Buat permintaan AJAX ke server untuk menghapus sesi
-            var xhr = new XMLHttpRequest();
-            xhr.open('GET', 'logout.php', true);
-            xhr.onreadystatechange = function () {
-                if (xhr.readyState == 4 && xhr.status == 200) {
-                    // Jika sesi dihapus dengan sukses, redirect ke halaman index.php
-                    window.location.href = 'index.php';
-                }
-            };
-            xhr.send();
-        }
-        
-
+        // Top Up Script
         $(document).ready(function () {
             $('#topUpForm').on('submit', function (event) {
                 event.preventDefault();
 
                 var amount = parseFloat($(this).find('input[name="inputAmount"]').val());
-
+                if (isNaN(amount) || amount <= 0) {
+                    $('#alertContainer').html('<div class="alert alert-danger alert-dismissible fade show" role="alert">Please enter a valid amount.</div>');
+                    return false;
+                }
                 var formattedAmount = new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(amount);
 
                 $('#totalAmount').text(formattedAmount);
 
 
                 amount = document.getElementById('inputAmount').value;
-                console.log (amount);
+                console.log(amount);
                 $('#paymentDetails').show();
                 $('#amount').val(amount);
 
@@ -421,63 +398,29 @@ if (isset($_SESSION['user'])) {
 
                     },
                     error: function (xhr, status, error) {
-                        // Tindakan jika penyimpanan perubahan gagal
-                        console.error('Terjadi kesalahan: ' + error);
-                    }
-                });
-            });
-
-            $('#confirmForm').on('submit', function () {
-                // Continue with the form submission
-                $.ajax({
-                    type: 'post',
-                    url: 'request_saldo.php',
-                    data: $('#topUpForm').serialize(),
-                    success: function (response) {
-                        $('#topUpForm').find('input[name="amount"]').prop('disabled', false);
-                        $('#topUpForm').find('input[name="login-btn"]').prop('disabled', false);
-
-                        $('#balanceInfo').modal('hide');
-                        alert('Permintaan isi saldo sudah dikirim ke admin kami, harap tunggu beberapa saat :).');
-                        location.reload();
-                    },
-                    error: function (xhr, status, error) {
-                        // Re-enable the form fields and "Pay" button in case of an error
-                        $('#topUpForm').find('input[name="amount"]').prop('disabled', false);
-                        $('#topUpForm').find('input[name="login-btn"]').prop('disabled', false);
-
-                        // Actions if saving changes fails
                         console.error('Terjadi kesalahan: ' + error);
                     }
                 });
             });
         });
 
-    </script>
-
-    <!-- Copy Button Script -->
-    <script>
-        // Select elements
+        //Copy Button Script
         const target = document.getElementById('copy1');
         const button = target.nextElementSibling;
-
         const clipboard = new ClipboardJS(button);
-
-        // Success action handler
         clipboard.on('success', function (e) {
             var checkIcon = button.querySelector('.ki-check');
             var copyIcon = button.querySelector('.ki-copy');
-
-            // Exit check icon when already showing
             if (checkIcon) {
                 return;
             }
         });
-    </script>
 
-    <!--Script Animation Card -->
-    <script>
+        $('#balanceInfo').on('hidden.bs.modal', function () {
+            location.reload();
+        });
 
+        //Script Animation Card 
         document.addEventListener("DOMContentLoaded", function () {
             var options = {
                 root: null,
@@ -502,9 +445,11 @@ if (isset($_SESSION['user'])) {
         });
     </script>
 
+    <!-- Bootstrap core JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 
-    <!--Script auto next script -->
     <script>
+        //Script auto next script
         document.addEventListener('DOMContentLoaded', function () {
             var carousel = document.getElementById('carouselExampleIndicators');
             var carouselInstance = new bootstrap.Carousel(carousel, {
@@ -518,9 +463,12 @@ if (isset($_SESSION['user'])) {
             setInterval(nextSlide, 2500);
         });
     </script>
+
+
 </body>
 
-<footer class="py-5 bg-dark" style="background-color: rgb(10, 10, 12) !important">
+<!-- Footer-->
+<footer class="py-5" style="background-color: rgb(0, 0, 0, 0.4) !important">
     <div class="container">
         <p class="m-0 text-center text-white">Copyright &copy; PAY2WIN 2023</p>
     </div>
