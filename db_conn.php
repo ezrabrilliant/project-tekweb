a5350a66a7554c03cc4009adb0f5a2c36c5bb488
Ezra Brilliant <64943463+ezrabrilliant@users.noreply.github.com>
2023-12-03 03:18:20 +0700

    Update db_conn.php (Using database from Telematics)

--- db_conn.php
+++ db_conn.php
@@ -1,8 +1,13 @@
-<!--
-    password samakan aja 'infor12345' biar gampang
--->
 <?php
-    $username = 'localhost';
-    $password = 'infor12345';
-    $pdo = new PDO('mysql:host=localhost;dbname=proyek_tekweb', $username, $password);
+    $host = 'telematics.petra.ac.id';
+    $dbname = 'tekweb05';
+    $username = '';
+    $password = '';
+    try {
+        $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
 ?>
